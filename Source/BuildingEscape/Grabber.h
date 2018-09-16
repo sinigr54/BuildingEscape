b8b9d2bfecb0b02070b47fd59df7f50cceacf794
Александр Чех <heapify@yandex.ru>
2018-09-16 17:27:10 +0300

    Add Grabber actor component

--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -4,18 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Engine/TriggerVolume.h"
-#include "OpenDoor.generated.h"
+#include "Grabber.generated.h"
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UOpenDoor();
+	UGrabber();
 
 protected:
 	// Called when the game starts
@@ -25,24 +24,6 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-
-private:
-
-	bool bOpenDoor{ true };
-
-	UPROPERTY(VisibleAnywhere)
-	float OpenAngle = 90.0f;
-
-	UPROPERTY(EditAnywhere)
-	ATriggerVolume* PressurePlate;
-
-	AActor* ActorThatOpens;
-
-private:
-
-	void OpenDoor();
-
-	void CloseDoor();
-
-	inline void OpenDoorAnimated();
+		
+	
 };
--- Source/BuildingEscape/OpenDoor.cpp
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -4,75 +4,65 @@
 
 
 // Sets default values for this component's properties
-UOpenDoor::UOpenDoor()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+UOpenDoor::UOpenDoor() {
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+    // ...
 }
 
 
 // Called when the game starts
-void UOpenDoor::BeginPlay()
-{
-	Super::BeginPlay();
+void UOpenDoor::BeginPlay() {
+    Super::BeginPlay();
 
     FString ObjectName = GetOwner()->GetName();
     UE_LOG(LogTemp, Warning, TEXT("Object name is: %s"), *ObjectName);
 
-	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+    ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
 // Called every frame
-void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (PressurePlate != nullptr && PressurePlate->IsOverlappingActor(ActorThatOpens))
-	{
-		OpenDoorAnimated();
-	}
+    if (PressurePlate != nullptr && PressurePlate->IsOverlappingActor(ActorThatOpens)) {
+        OpenDoorAnimated();
+    }
 }
 
-void UOpenDoor::OpenDoor()
-{
-	AActor* Owner = GetOwner();
-	FRotator RotationTransform = Owner->GetActorRotation();
+void UOpenDoor::OpenDoor() {
+    AActor *Owner = GetOwner();
+    FRotator RotationTransform = Owner->GetActorRotation();
 
-	RotationTransform.Yaw = -OpenAngle;
-	Owner->SetActorRotation(RotationTransform, ETeleportType::TeleportPhysics);
+    RotationTransform.Yaw = -OpenAngle;
+    Owner->SetActorRotation(RotationTransform, ETeleportType::TeleportPhysics);
 }
 
 void UOpenDoor::OpenDoorAnimated() {
-	AActor* Owner = GetOwner();
-	FRotator RotationTransform = Owner->GetActorRotation();
+    AActor *Owner = GetOwner();
+    FRotator RotationTransform = Owner->GetActorRotation();
 
-	if (bOpenDoor)
-	{
-		if (RotationTransform.Yaw > -40)
-		{
-			RotationTransform.Yaw -= 0.5;
-			Owner->SetActorRotation(RotationTransform, ETeleportType::TeleportPhysics);
-		}
-		else
-		{
-			bOpenDoor = false;
-		}
-	}
-	else
-	{
-		if (RotationTransform.Yaw < 0)
-		{
-			RotationTransform.Yaw += 0.5;
-			Owner->SetActorRotation(RotationTransform, ETeleportType::TeleportPhysics);
-		}
-		else
-		{
-			bOpenDoor = true;
-		}
-	}
+    if (bOpenDoor) {
+        if (RotationTransform.Yaw > -40) {
+            RotationTransform.Yaw -= 0.5;
+            Owner->SetActorRotation(RotationTransform, ETeleportType::TeleportPhysics);
+        } else {
+            bOpenDoor = false;
+        }
+    } else {
+        if (RotationTransform.Yaw < 0) {
+            RotationTransform.Yaw += 0.5;
+            Owner->SetActorRotation(RotationTransform, ETeleportType::TeleportPhysics);
+        } else {
+            bOpenDoor = true;
+        }
+    }
+}
+
+void UOpenDoor::CloseDoor() {
+
 }
 
