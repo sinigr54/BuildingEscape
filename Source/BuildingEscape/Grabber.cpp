ab02979bf12793ee2ba013de9bf4f22742df3cc0
Александр Чех <heapify@yandex.ru>
2018-09-22 21:38:51 +0300

    Partially add Grabber implementation

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -1,34 +1,110 @@
 // All rights reserved. MOBILEDEV.
 
 #include "Grabber.h"
+#include <Public/DrawDebugHelpers.h>
+#include <Public/CollisionQueryParams.h>
 
+#include <Engine/World.h>
+#include <GameFramework/Actor.h>
+#include <GameFramework/PlayerController.h>
+
+#define OUT
 
 // Sets default values for this component's properties
-UGrabber::UGrabber()
-{
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+UGrabber::UGrabber() {
+    // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+    // off to improve performance if you don't need them.
+    PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+    // ...
 }
 
 
 // Called when the game starts
-void UGrabber::BeginPlay()
-{
-	Super::BeginPlay();
+void UGrabber::BeginPlay() {
+    Super::BeginPlay();
 
-	// ...
-	
+    FindPhysicsHandleComponent();
+    SetUpInputComponent();
 }
 
 
 // Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+
+}
+
+void UGrabber::Grab() {
+    UE_LOG(LogTemp, Warning, TEXT("Player Grab key pressed!"))
+
+    GetFirstPhysicsBodyInReach();
+}
+
+void UGrabber::Release() {
+    UE_LOG(LogTemp, Warning, TEXT("Player Grab key released!"))
+}
+
+void UGrabber::FindPhysicsHandleComponent() {
+    /// Look for attached Physics Handle
+    PhysicsHandleComponent = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+    if (PhysicsHandleComponent == nullptr) {
+        UE_LOG(LogTemp, Error, TEXT("Object %s don't have PhysicsHandleComponent component. Add it at UA editor!"),
+               *GetOwner()->GetName()
+        )
+    }
+}
+
+void UGrabber::SetUpInputComponent() {
+    /// Look for attached Input Component
+    InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+    if (InputComponent == nullptr) {
+        UE_LOG(LogTemp, Error, TEXT("Object %s don't have InputComponent component. Add it at UA editor!"),
+               *GetOwner()->GetName()
+        )
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("Input Component found!"))
+
+        /// Bind the input axis
+        InputComponent->BindAction("Grab", EInputEvent::IE_Pressed, this, &UGrabber::Grab);
+        InputComponent->BindAction("Grab", EInputEvent::IE_Released, this, &UGrabber::Release);
+    }
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const {
+
+    // Get player view point this tick
+    FVector PlayerViewPointLocation;
+    FRotator PlayerViewPointRotation;
+
+    GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+            OUT PlayerViewPointLocation,
+            OUT PlayerViewPointRotation
+    );
+
+    FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+    /// Setup query parameters
+    FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+    /// Line-trace (AKA ray-cast) out to reach distance
+    FHitResult Hit;
+    GetWorld()->LineTraceSingleByObjectType(
+            OUT Hit,
+            PlayerViewPointLocation,
+            LineTraceEnd,
+            FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+            TraceParameters
+    );
+
+    AActor *actor = Hit.GetActor();
+    if (actor != nullptr) {
+        UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"),
+               *(actor->GetName())
+        )
+    }
+
+    return Hit;
 }
 
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -4,26 +4,43 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+
+#include <PhysicsEngine/PhysicsHandleComponent.h>
+#include <Components/InputComponent.h>
+
 #include "Grabber.generated.h"
 
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BUILDINGESCAPE_API UGrabber : public UActorComponent {
+    GENERATED_BODY()
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class BUILDINGESCAPE_API UGrabber : public UActorComponent
-{
-	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UGrabber();
+public:
+    // Sets default values for this component's properties
+    UGrabber();
 
 protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
+    // Called when the game starts
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+    // Called every frame
+    virtual void
+    TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
 
-		
-	
+private:
+
+    float Reach{100.0f};
+
+    UPhysicsHandleComponent *PhysicsHandleComponent{nullptr};
+    UInputComponent *InputComponent{nullptr};
+
+    void Grab();
+
+    void Release();
+
+    void FindPhysicsHandleComponent();
+
+    void SetUpInputComponent();
+
+    FHitResult GetFirstPhysicsBodyInReach() const;
 };
